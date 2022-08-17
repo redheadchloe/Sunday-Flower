753c68f0c0eba9062d0ad7d4220c1481b8ac579e
redheadchloe <chloe.socrates.7z@gmail.com>
2022-08-18 01:04:13 +0900

    update

--- script.js
+++ script.js
@@ -17,11 +17,12 @@ open.addEventListener('click', function (e) {
 
 // dropdown menu
 const dropBtn = document.getElementsByClassName('drop-btn');
-var i;
 
+var i;
 for (i = 0; i < dropBtn.length; i++) {
-    dropBtn[i].addEventListener("click", function (e) {
+    dropBtn[i].addEventListener("click", e => {
         const menus = document.querySelectorAll('.dropdown');
+        console.log(e.target);
         menus.forEach(function (menu) {
             if (menu === e.currentTarget.nextElementSibling) {
                 menu.classList.toggle('on');
@@ -34,8 +35,8 @@ for (i = 0; i < dropBtn.length; i++) {
 }
 
 // click anywhere to hide the dropdown
-window.onclick = function (event) {
-    if (!event.target.matches('.drop-btn')) {
+window.onclick = function (e) {
+    if (!e.target.matches('.drop-btn')) {
         var dropdowns = document.getElementsByClassName('dropdown');
         var i;
         for (i = 0; i < dropdowns.length; i++) {
@@ -45,7 +46,27 @@ window.onclick = function (event) {
             }
         }
     }
+    if (!e.target.matches('.results') && results.classList.contains('active')) {
+        results.classList.remove('active');
+    }
 }
+
+// search
+const input = document.querySelector('.search');
+results = document.querySelector('.results')
+input.addEventListener('input', e => {
+    text = e.target.value;
+    console.log(text);
+    if (text && text.trim().length > 0) {
+        text = text.trim().toLowerCase();
+        results.classList.add('active');
+        results.innerHTML = `「${text}」に一致する検索結果：`;
+    } else {
+        results.classList.remove('active');
+    }
+})
+
+
 
 // back to top
 const back = document.querySelector('.to_top');
